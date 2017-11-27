275dc1919c3ccaeff77436adec7aea37e4308522
Sharmina Akhtar <sakht318@gmail.com>
2017-11-27 11:48:10 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize (word)
+    @word = word
+  end
+
+  def match (words)
+    word_split = @word.split("").sort
+    words.find_all do |w|
+      anagram = w.split("").sort
+      anagram == word_split
+    end
+  end
+
+end
