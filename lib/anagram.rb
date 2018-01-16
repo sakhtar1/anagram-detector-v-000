ce180d091e9555cc4e69fba88338729b3f02246c
Sharmina Akhtar <sakht318@gmail.com>
2018-01-16 06:28:12 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,13 +1,13 @@
 # Your code goes here!
 class Anagram
-  
+
   attr_accessor :detect
   anagram = []
-  
+
   def initialize(detect)
     @detect = detect
   end
-  
+
   def anagram(word)
     word = @detect.split("").sort
     detect.find_all do |w|
@@ -15,7 +15,7 @@ class Anagram
       anagram.word == word
     end
   end
-    
-  
+
+
 
 end
